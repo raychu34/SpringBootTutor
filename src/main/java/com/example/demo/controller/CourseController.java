b82e4601381ef601a1e689c44b453b455dda7276
Ray Chu <rayallen1113.ee93@gmail.com>
2019-09-14 21:35:48 -0400

    two sum

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -9,11 +9,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.*;
+
 
 // single function interface
 @RestController
@@ -45,6 +49,31 @@ public class CourseController {
 
         return new ResponseEntity(findedCourse, HttpStatus.OK);
     }
+
+    @PostMapping("/two-sum")
+    public ResponseEntity twoSum(
+            @RequestBody @RequestParam("target") Integer target,
+            @RequestBody @RequestParam("numbers") String nums) {
+        try {
+            List<String> numbers = new ArrayList<>(Arrays.asList(nums.split(",")));
+            HashMap<Integer, Integer> map = new HashMap<>();
+            List<Integer> result = new ArrayList<>();
+            for (int i = 0; i < numbers.size(); i++) {
+                int num = Integer.valueOf(numbers.get(i));
+                if (map.get(num) != null) {
+                    result.add(map.get(num));
+                    result.add(i);
+                }
+                map.put(target - num, i);
+            }
+
+            return new ResponseEntity(result, HttpStatus.OK);
+        }
+        catch(java.lang.NumberFormatException e) {
+            return new ResponseEntity("bad parameter", HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
 
 // 增加一个课程/删除一个课程/更新一个课程信息
